993190896d420f575fdd2990293bddc6caf3ca39
mireyaavila <Bubblegum2408*>
2023-08-21 20:42:29 -0500

    babel.config file updated

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,17 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo','@babel/preset-env', '@babel/preset-react','module:metro-react-native-babel-preset', '@babel/preset-flow'],
-    plugins: ['react-native-reanimated/plugin'],
+    presets: [
+      'babel-preset-expo',
+      ['@babel/preset-env', { 'loose': true }],
+      '@babel/preset-react',
+      'module:metro-react-native-babel-preset',
+      '@babel/preset-flow'
+    ],
+    plugins: [
+      'react-native-reanimated/plugin',
+      ['@babel/plugin-transform-private-methods', { 'loose': true }],
+      ['@babel/plugin-transform-private-property-in-object', { 'loose': true }]
+    ],
   };
 };
